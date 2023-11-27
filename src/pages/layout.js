3d3336effbbc5b1ac5b2a77b3862ef4726abdb53
Ahmet Kemal Türker <ahmet.turker@ogr.gelisim.edu.tr>
2023-11-27 12:15:08 +0300

    pc de son kalan

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
-    return (
+    return ( 
         <html lang='en'>
             <body className={inter.className}>{children}</body>
         </html>
